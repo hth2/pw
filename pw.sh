a7c5786a4f80aaa51c85d91829c8bc4a33fdf72b
hth2 <hanthethanh@gmail.com>
2019-09-25 12:52:14 +0200

    update: fix type pw-search_file, print command before running

--- pw.sh
+++ pw.sh
@@ -39,105 +39,110 @@ set_pw_platform /usr/local/bin/brew    not_set homebrew
 # use sudo if available
 SUDO=$(command -v sudo 2>/dev/null)
 
+run_cmd() {
+  echo "$@"
+  "$@"
+}
+
 
 pw_self_update() {
   set -x
-  curl -L https://raw.githubusercontent.com/hth2/pw/master/pw.sh -o /usr/local/bin/pw.sh
+  run_cmd $SUDO curl -L https://raw.githubusercontent.com/hth2/pw/master/pw.sh -o "$0"
   set +x
 }
 
 # some common and fallback utils:
 func_undefined()  { echo "$1" is undefined; exit 1; }
 
-pw_install()      { func_undefined "$FUNCNAME"; }
-pw_remove()       { func_undefined "$FUNCNAME"; }
-pw_clean()        { func_undefined "$FUNCNAME"; }
+pw_install()      { func_undefined "${FUNCNAME[0]}"; } # DONE
+pw_remove()       { func_undefined "${FUNCNAME[0]}"; }
+pw_clean()        { func_undefined "${FUNCNAME[0]}"; }
 
-pw_search()       { func_undefined "$FUNCNAME"; }
-pw_search_desc()  { func_undefined "$FUNCNAME"; }
-pw_search_file()  { func_undefined "$FUNCNAME"; }
+pw_search()       { func_undefined "${FUNCNAME[0]}"; }
+pw_search_desc()  { func_undefined "${FUNCNAME[0]}"; }
+pw_search_file()  { func_undefined "${FUNCNAME[0]}"; }
 
-pw_update()       { func_undefined "$FUNCNAME"; }
-pw_upgrade()      { func_undefined "$FUNCNAME"; }
+pw_update()       { func_undefined "${FUNCNAME[0]}"; }
+pw_upgrade()      { func_undefined "${FUNCNAME[0]}"; }
 
-pw_source()       { func_undefined "$FUNCNAME"; }
-pw_builddep()     { func_undefined "$FUNCNAME"; }
-pw_download()     { func_undefined "$FUNCNAME"; }
+pw_source()       { func_undefined "${FUNCNAME[0]}"; }
+pw_builddep()     { func_undefined "${FUNCNAME[0]}"; }
+pw_download()     { func_undefined "${FUNCNAME[0]}"; }
 
-pw_info()         { func_undefined "$FUNCNAME"; }
-pw_check()        { func_undefined "$FUNCNAME"; }
-pw_file()         { func_undefined "$FUNCNAME"; }
-pw_list()         { func_undefined "$FUNCNAME"; }
-pw_own()          { func_undefined "$FUNCNAME"; }
-pw_versions()     { func_undefined "$FUNCNAME"; }
+pw_info()         { func_undefined "${FUNCNAME[0]}"; }
+pw_check()        { func_undefined "${FUNCNAME[0]}"; }
+pw_file()         { func_undefined "${FUNCNAME[0]}"; }
+pw_list()         { func_undefined "${FUNCNAME[0]}"; }
+pw_own()          { func_undefined "${FUNCNAME[0]}"; }
+pw_versions()     { func_undefined "${FUNCNAME[0]}"; }
 
 
 case "$PW_PACKAGE_FORMAT" in
 deb)
-    pw_list()   { dpkg-query -f '${binary:Package}\n' -W; }
-    pw_info()   { dpkg -p "$@"; }
-    pw_check()  { dpkg -s "$@"; }
-    pw_file()   { dpkg -L "$@"; }
-    pw_own()    { dpkg -S "$@"; }
+    pw_list()   { run_cmd dpkg-query -f '${binary:Package}\n' -W; }
+    pw_info()   { run_cmd dpkg -p "$@"; }
+    pw_check()  { run_cmd dpkg -s "$@"; }
+    pw_file()   { run_cmd dpkg -L "$@"; }
+    pw_own()    { run_cmd dpkg -S "$@"; }
     ;;
 
 rpm)
-    pw_info()   { rpm -qi "$@";  }
-    pw_check()  { rpm -q "$@";   }
-    pw_file()   { rpm -ql "$@";  }
-    pw_list()   { rpm -qa;       }
-    pw_own()    { rpm -qf "$@";  }
+    pw_info()   { run_cmd rpm -qi "$@";  }
+    pw_check()  { run_cmd rpm -q "$@";   }
+    pw_file()   { run_cmd rpm -ql "$@";  }
+    pw_list()   { run_cmd rpm -qa;       }
+    pw_own()    { run_cmd rpm -qf "$@";  }
     ;;
 esac
 
 case "$PW_PACKAGE_MANAGER" in
 apt)
-    pw_install()       { $SUDO apt-get install --no-install-recommends "$@"; }
-    pw_remove()        { $SUDO apt-get --purge purge "$@"; }
-    pw_update()        { $SUDO apt-get update; }
-    pw_upgrade()       { $SUDO apt-get update && apt-get dist-upgrade; }
+    pw_install()       { run_cmd $SUDO apt-get install --no-install-recommends "$@"; }
+    pw_remove()        { run_cmd $SUDO apt-get --purge purge "$@"; }
+    pw_update()        { run_cmd $SUDO apt-get update; }
+    pw_upgrade()       { run_cmd $SUDO apt-get update && apt-get dist-upgrade; }
     # ref: https://askubuntu.com/questions/194651/why-use-apt-get-upgrade-instead-of-apt-get-dist-upgrade
-    pw_clean()         { $SUDO apt-get clean; }
-    pw_info()          { apt-cache show "$@"; }
+    pw_clean()         { run_cmd $SUDO apt-get clean; }
+    pw_info()          { run_cmd apt-cache show "$@"; }
 
-    # pw_search()        { apt-cache search --names-only "$@"; }
-    pw_search()        { apt search "$@"; }
-    pw_search_desc()   { apt-cache search "$@"; }
-    pw_search_file()   { apt-file search "$@"; }
+    # pw_search()        { run_cmd apt-cache search --names-only "$@"; }
+    pw_search()        { run_cmd apt search "$@"; }
+    pw_search_desc()   { run_cmd apt-cache search "$@"; }
+    pw_search_file()   { run_cmd apt-file search "$@"; }
 
-    pw_source()        { apt-get source "$@"; }
-    pw_builddep()      { apt-get build-dep "$@"; }
-    pw_download()      { apt-get install --download-only "$@"; }
-    pw_version()       { apt-cache policy "$@"; }
+    pw_source()        { run_cmd apt-get source "$@"; }
+    pw_builddep()      { run_cmd apt-get build-dep "$@"; }
+    pw_download()      { run_cmd apt-get install --download-only "$@"; }
+    pw_version()       { run_cmd apt-cache policy "$@"; }
     ;;
 
 homebrew)
-    pw_install()       { brew install "$@"; }
-    pw_remove()        { brew uninstall "$@"; }
-    pw_update()        { brew update; }
-    pw_upgrade()       { brew upgrade "$@";  }
+    pw_install()       { run_cmd brew install "$@"; }
+    pw_remove()        { run_cmd brew uninstall "$@"; }
+    pw_update()        { run_cmd brew update; }
+    pw_upgrade()       { run_cmd brew upgrade "$@";  }
 
-    pw_search()        { brew search --name "$@"; }
-    pw_search_desc()   { brew search --description "$@"; }
+    pw_search()        { run_cmd brew search --name "$@"; }
+    pw_search_desc()   { run_cmd brew search --description "$@"; }
 
-    pw_info()          { brew info "$@"; }
-    pw_file()          { brew list --verbose "$@"; }
-    pw_list()          { brew list; }
+    pw_info()          { run_cmd brew info "$@"; }
+    pw_file()          { run_cmd brew list --verbose "$@"; }
+    pw_list()          { run_cmd run_cmd brew list; }
     ;;
 
 zypper)
-    pw_install()       { zypper install "$@"; }
-    pw_remove()        { zypper remove "$@"; }
-    pw_update()        { zypper refresh; }
-    pw_upgrade()       { zypper update; }
-    pw_clean()         { zypper clean; }
+    pw_install()       { run_cmd zypper install "$@"; }
+    pw_remove()        { run_cmd zypper remove "$@"; }
+    pw_update()        { run_cmd zypper refresh; }
+    pw_upgrade()       { run_cmd zypper update; }
+    pw_clean()         { run_cmd zypper clean; }
 
-    pw_search()        { zypper search "$@"; }
-    pw_search_desc()   { zypper search "$@"; }
+    pw_search()        { run_cmd zypper search "$@"; }
+    pw_search_desc()   { run_cmd zypper search "$@"; }
 
-    pw_source()        { zypper source-install "$@"; }
-    pw_builddep()      { zypper source-install -d "$@"; }
-    pw_download()      { zypper --download-only "$@"; }
+    pw_source()        { run_cmd zypper source-install "$@"; }
+    pw_builddep()      { run_cmd zypper source-install -d "$@"; }
+    pw_download()      { run_cmd zypper --download-only "$@"; }
     ;;
 
 
@@ -156,8 +161,8 @@ alias pw-info='pw_info'
 alias pw-list='pw_list'
 alias pw-own='pw_own'
 alias pw-search='pw_search'
-alias pw-search_desc='pw_search_desc'
-alias pw-search_file='pw_search_file'
+alias pw-search-desc='pw_search_desc'
+alias pw-search-file='pw_search_file'
 alias pw-update='pw_update'
 alias pw-upgrade='pw_upgrade'
 alias pw-source='pw_source'
